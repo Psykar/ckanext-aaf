61f5f88cae51652a22c7601557c1cb5ad6312e7a
Louis des Landes <ldeslandes@swin.edu.au>
2016-04-07 10:39:30 +1000

    Update version to 0.0.2

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
-    # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.1',
+    # https://packaging.python.org/en/latest/single_source_version/#single-sourcing-the-version
+    version='0.0.2',
 
     description='''Authentication via AAF Rapid Connect''',
     long_description=long_description,
